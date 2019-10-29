e99cbd23e15db2599ae6376dac524d5b5ed2146b
gchristie <gchristie@group1auto.com>
2019-10-29 11:58:49 -0500

    2 files modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -1,39 +1,48 @@
 export var racedata = [
      
    {
+    runner: 'Gordon',
     name: 'Marine Corps Marathon',
     racedate: '10/27/2019',
     racetype: 'National Marathon',
-    racetime: '05:58',
-    runner: 'Gordon',
-    photoURL: 'http://marathonfoto.com/Proofs?PIN=Y9V558&LastName=CHRISTIE'
+    bibnumber: '3740',
+    racetime: '05:58:34',
+    resultsurl:'https://events.hakuapp.com/events/1405901239e66d083c42/live_tracking_results?registration_number=8D1708400CA3',
+    photourl: 'http://marathonfoto.com/Proofs?PIN=Y9V558&LastName=CHRISTIE'
   }
   ,
   {
+    runner: 'Gordon',
     name: 'Run 4 The Children',
     racedate: '11/02/2019',
     racetype: '4 Miles',
-    racetime: '00:00',
-    runner: 'Gordon',
-    photoURL: ''
+    bibnumber: "",
+    racetime: '',
+    resultsurl:'',
+    photourl: ''
   }
    ,
    {
+    runner: 'Gordon',
     name: 'Snowdrop 55 Hour Ultra',
     racedate: '12/30/2019',
     racetype: '100+Miles',
-    racetime: '00:00',
-    runner: 'Gordon',
-    photoURL: ''
+    bibnumber: "",
+    racetime: '',
+    resultsurl:'',
+
+    photourl: ''
   }
  ,
    {
+    runner: 'Rosa',
     name: 'Run 4 The Children',
     racedate: '11/02/2019',
     racetype: '4 Miles',
-    racetime: '00:00',
-    runner: 'Rosa',
-    photoURL: 'PHOTOS'
+    bibnumber: "",
+    racetime: '',
+    resultsurl:'',
+    photourl: 'PHOTOS'
   }
 
 ];
