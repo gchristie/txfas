c8467989c233b6061a65586165a068517710b902
gchristie <gchristie@group1auto.com>
2020-01-10 09:27:23 -0600

    1 file modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -1,85 +1,131 @@
 export var racedata = [
-     
-   {
-    runner: 'Gordon',
-    name: 'Marine Corps Marathon',
-    racedate: '10/27/2019',
-    racetype: 'National Marathon',
-    bibnumber: '3740',
-    racetime: '05:58:34',
-    resultsurl:'https://events.hakuapp.com/events/1405901239e66d083c42/live_tracking_results?registration_number=8D1708400CA3',
-    photourl: 'http://marathonfoto.com/Proofs?PIN=Y9V558&LastName=CHRISTIE',
-    status: 'Show'
-  }
-  ,
-  {
-    runner: 'Gordon',
-    name: 'Run 4 The Children',
-    racedate: '11/02/2019',
-    racetype: '4 Miles',
-    bibnumber: '437',
-    racetime: '53:56.7',
-    resultsurl:'https://www.runhoustontiming.net/race_result/1572718099_3745642_Overall.htm',
-    photourl: 'https://runsignup.com/Race/Photos/40578/110511/437',
-    status:'Show'
-  }
-   ,
-   {
-    runner: 'Gordon',
-    name: 'Snowdrop 55 Hour Ultra',
-    racedate: '12/30/2019',
-    racetype: '55 Hour Solo',
-    bibnumber: '70',
-    racetime: '17 miles',
-    resultsurl:'https://my.raceresult.com/145480/Live?lang=en#1_BEC6FF',
-    photourl: 'https://photos.app.goo.gl/8UidbZYbVsxjAkwF8',
-    status:''
-  }
- ,
-   {
-    runner: 'Rosa',
-    name: 'Run 4 The Children',
-    racedate: '11/02/2019',
-    racetype: '4 Miles',
-    bibnumber: '438',
-    racetime: '53:58.9',
-    resultsurl:'https://www.runhoustontiming.net/race_result/1572718099_3745642_Overall.htm',
-    photourl: 'https://runsignup.com/Race/Photos/40578/110511/438',
-    status:'Show'
-  }
-
-,
+//Gordon Data
 {
-runner:'Rosa',
-name:"Rock 'n' Roll Las Vegas",
-racedate:'11/17/2019',
-racetype:'Half Marathon',
-bibnumber:'1406',
-racetime:'03:45:17',
-resultsurl:'https://connect.garmin.com/modern/activity/4262218892',
-photourl:'https://www.finisherpix.com/gallery/photos/en/USD/3393/1406',
-status:'Show'
+runner: 'Gordon', status: 'Show',
+name: 'Marine Corps Marathon', racedate: '10/27/2019', racetype: 'National Marathon', bibnumber: '3740',    racetime: '05:58:34',
+resultsurl:'https://events.hakuapp.com/events/1405901239e66d083c42/live_tracking_results?registration_number=8D1708400CA3',
+photourl: 'http://marathonfoto.com/Proofs?PIN=Y9V558&LastName=CHRISTIE'
 }
 ,
-   {
-    runner: 'Rosa',
-    name: 'Snowdrop 55 Hour Ultra Relay',
-    racedate: '12/30/2019',
-    racetype: '55 Hour Relay',
-    bibnumber: '371',
-    racetime: '207 Miles',
-    resultsurl:'https://my.raceresult.com/145480/results?lang=en#2_70740E',
-    photourl: 'https://photos.app.goo.gl/jkBHYAtgYdTPKC5P6',
-    status:''
-  }
+{
+runner: 'Gordon', status:'Show',
+name: 'Run 4 The Children', racedate: '11/02/2019', racetype: '4 Miles', bibnumber: '437', racetime: '53:56.7',
+resultsurl:'https://www.runhoustontiming.net/race_result/1572718099_3745642_Overall.htm',
+photourl: 'https://runsignup.com/Race/Photos/40578/110511/437'
+}
+,
+{
+runner: 'Gordon', status:'',
+name: 'Snowdrop 55 Hour Ultra',    racedate: '12/30/2019',    racetype: '55 Hour Solo', bibnumber: '70', racetime: '17 miles',   
+resultsurl:'https://my.raceresult.com/145480/Live?lang=en#1_BEC6FF',   
+photourl: 'https://photos.app.goo.gl/8UidbZYbVsxjAkwF8'
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'ABB 5K',    racedate: '01/01/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'Houston Marathon',    racedate: '01/19/2020',    racetype: 'Marathon', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'Hot Chocolate 5K',    racedate: '02/01/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'Wonder Woman 5K',    racedate: '04/05/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'Art Car 5K',    racedate: '04/05/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: 'Boston Marathon',    racedate: '04/20/2020',    racetype: 'National Marathon', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Gordon', status:'Future',
+name: '',    racedate: '',    racetype: '', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: '.'
+}
 
-
+//Rosa Data
+,
+{
+runner: 'Rosa', status:'Show',
+name: 'Run 4 The Children',racedate: '11/02/2019',racetype: '4 Miles',bibnumber: '438',racetime: '53:58.9',
+resultsurl:'https://www.runhoustontiming.net/race_result/1572718099_3745642_Overall.htm',
+photourl: 'https://runsignup.com/Race/Photos/40578/110511/438'
+}
+,
+{
+runner:'Rosa',status:'Show',
+name:"Rock 'n' Roll Las Vegas",racedate:'11/17/2019',racetype:'Half Marathon',bibnumber:'1406',racetime:'03:45:17',
+resultsurl:'https://connect.garmin.com/modern/activity/4262218892',
+photourl:'https://www.finisherpix.com/gallery/photos/en/USD/3393/1406'
+}
+,
+{
+runner: 'Rosa', status:'',
+name: 'Snowdrop 55 Hour Ultra Relay', racedate: '12/30/2019', racetype: '55 Hour Relay', bibnumber: '371', racetime: '207 Miles',
+resultsurl:'https://my.raceresult.com/145480/results?lang=en#2_70740E',
+photourl: 'https://photos.app.goo.gl/jkBHYAtgYdTPKC5P6'
+}
+,
+{
+runner: 'Rosa', status:'Future',
+name: 'ABB 5K',    racedate: '01/18/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Rosa', status:'Future',
+name: 'Hot Chocolate 5K',    racedate: '02/01/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Rosa', status:'Future',
+name: 'Wonder Woman 5K',    racedate: '04/05/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Rosa', status:'Future',
+name: 'Art Car 5K',    racedate: '04/05/2020',    racetype: '5K', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: ''
+}
+,
+{
+runner: 'Rosa', status:'Future',
+name: '',    racedate: '',    racetype: '', bibnumber: '', racetime: '',   
+resultsurl:'',
+photourl: '.'
+}
 
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
