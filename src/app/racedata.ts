6e9a1fc5fdaa9b7b920040d34697bb93ab7280dc
gchristie <gchristie@group1auto.com>
2020-01-20 11:19:24 -0600

    1 file modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -1,14 +1,14 @@
 export var racedata = [
 //Gordon Data
 {
-runner: 'Gordon', status: 'Show',
+runner: 'Gordon', status: 'Hide',
 name: 'Marine Corps Marathon', racedate: '10/27/2019', racetype: 'National Marathon', bibnumber: '3740',    racetime: '05:58:34',
 resultsurl:'https://events.hakuapp.com/events/1405901239e66d083c42/live_tracking_results?registration_number=8D1708400CA3',
 photourl: 'http://marathonfoto.com/Proofs?PIN=Y9V558&LastName=CHRISTIE'
 }
 ,
 {
-runner: 'Gordon', status:'Show',
+runner: 'Gordon', status:'Hide',
 name: 'Run 4 The Children', racedate: '11/02/2019', racetype: '4 Miles', bibnumber: '437', racetime: '53:56.7',
 resultsurl:'https://www.runhoustontiming.net/race_result/1572718099_3745642_Overall.htm',
 photourl: 'https://runsignup.com/Race/Photos/40578/110511/437'
